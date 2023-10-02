e8b6c8a94e3c277c53828156d5d726aa255c3966
KRAFTER341 <144213945+KRAFTER341@users.noreply.github.com>
2023-10-02 15:47:40 +0500

    Add files via upload

---
+++ puton/index.py
@@ -0,0 +1,20 @@
+# a = int(input("1число:"))
+# print(a)
+# print(a + 1)
+# print(a + 2)
+#
+
+
+#a = int(input("1число:"))
+#b = int(input("2число:"))
+#c = int(input("3число:"))
+#print(a + b + c)
+
+a = int(input("Длина ребра  "))
+v = a ** 3
+s = 6 * a ** 2
+print('Обьем =' , v )
+print('Плошадь полной поверхности=' , s)
+
+
+
